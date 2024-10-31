5bae08058b7f4a6b6c9104767e8746f984e86f60
iuliap710 <111219535+iuliap710@users.noreply.github.com>
2024-10-31 09:22:50 +0200

    I created CellType class.

---
+++ ProjectModernpp/CellType.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <cstdint>
+enum class CellType : uint8_t
+{
+	EMPTY, 
+	BREAKABLE_WALL, 
+	UNBREAKABLE_WALL
+};
--- ProjectModernpp/GameMap.h
+++ ProjectModernpp/GameMap.h
@@ -1,12 +1,17 @@
 #pragma once
+#include "CellType.h"
 #include <cstdint>
+#include <vector>
 
 class GameMap
 {
 private:
 	uint32_t width, height;
+	std::vector<std::vector<CellType>> grid;
 
 public:
-	GameMap(int w, int h) : width(w), height(h) {}
+	GameMap(int w, int h) : width(w), height(h) {
+		grid.resize(height, std::vector<CellType>(width, CellType::EMPTY));
+	}
 
 };
