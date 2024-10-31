afd4a7531e48ca483fe41f9faf93a0bfc1b58b04
nicol.stancu <nicol.stancu@student.unitbv.ro>
2024-10-31 17:01:23 +0200

    I wrote in the Bullet class and created Bullet.cpp.

---
+++ ProjectModernpp/Bullet.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "Bullet.h"
+
+const float Bullet::speed = 0.25f;
--- ProjectModernpp/Bullet.h
+++ ProjectModernpp/Bullet.h
@@ -1,5 +1,11 @@
 #pragma once
 
 class Bullet {
-
+private:
+	bool active = false;
+	static const float speed;
+	int x, y;
+public:
+	Bullet(const bool& active, const int& x, const int& y)
+		: active{ active }, x{ x }, y{ y } {}
 };
