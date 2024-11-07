6b1d256610f4d24381eba4596b962b1a337b1bdc
Ingrid2911 <ingrid.mihaita@gmail.com>
2024-11-07 14:45:02 +0200

    I wrote in the PlayWindow.h.

---
+++ ProjectModernpp/PlayWindow.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QMessageBox> // Pentru afișarea mesajelor de eroare
+class PlayWindow : public QWidget {
+    Q_OBJECT
+
+public:
+    PlayWindow(QWidget* parent = nullptr); // Constructor
+
+};
+
