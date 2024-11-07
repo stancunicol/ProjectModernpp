cb68d867e4801428dded3fa632dd4fb7c7f4e544
Ingrid2911 <ingrid.mihaita@gmail.com>
2024-11-07 14:47:57 +0200

    I wrote in PlayWindow.cpp.

--- ProjectModernpp/PlayWindow.cpp
+++ ProjectModernpp/PlayWindow.cpp
@@ -1,7 +1,17 @@
-#include "PlayWindow.h"
+﻿#include "PlayWindow.h"
 #include <QVBoxLayout>
 #include <QPushButton>
 #include<qpalette>
-PlayWindow::PlayWindow(QWidget* parent){
-    
+PlayWindow::PlayWindow(QWidget* parent)
+    : QWidget(parent) {
+    // Setare titlu și dimensiune fereastră
+    setWindowTitle("Generate Code");
+    setFixedSize(300, 200);
+
+
+    QPalette palette;
+    palette.setColor(QPalette::Window, Qt::black); // Fundal negru
+    palette.setColor(QPalette::WindowText, Qt::white); // Text alb
+    setPalette(palette);
+    setAutoFillBackground(true); // Asigură-te că fundalul se umple cu culoarea
 }
