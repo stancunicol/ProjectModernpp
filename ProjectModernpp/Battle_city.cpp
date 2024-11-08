2c514809999cfda75e093b3b9e6e214f35e5ea7a
Alessandra <vilcualessandra@gmail.com>
2024-11-08 16:28:25 +0200

    I added a background to the Battle_city window

--- ProjectModernpp/Battle_city.cpp
+++ ProjectModernpp/Battle_city.cpp
@@ -5,6 +5,16 @@ Battle_city::Battle_city(QWidget* parent)
 {
 	setWindowTitle("Battle City");//title
 	resize(1200, 700);//window 1500X775
+	background = new QLabel(this);
+	QPixmap pixmap("./Battle_city.jpg");
+	if (pixmap.isNull())
+	{
+		qDebug() << "The image couldn't be loaded!";
+	}
+	background->setPixmap(pixmap);//Set the image as a QPixmap for the QLabel
+	background->setScaledContents(true);//Scale the image to occupy the entire widget
+	background->setGeometry(this->rect());//Set the geometry of the QLabel to occupy the entire window
+	setCentralWidget(background);//Set the QLabel as the center widget
 }
 
 Battle_city::~Battle_city()
--- ProjectModernpp/Battle_city.h
+++ ProjectModernpp/Battle_city.h
@@ -3,6 +3,8 @@
 #include <QMainWindow>//standard main window for GUI applications
 #include <QLabel>//to display images or text
 #include <QPushButton>//create buttons
+#include <QPixmap>//to manage and manipulate images
+#include <QDebug>//debug in console
 
 class Battle_city : public QMainWindow
 {
