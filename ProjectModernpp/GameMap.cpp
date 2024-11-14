6ec2da175d1fc7e4a52fecb033d194d1aae01f95
iuliap710 <111219535+iuliap710@users.noreply.github.com>
2024-11-14 18:47:57 +0200

    I changed the way of creating the map, by level.

--- ProjectModernpp/GameMap.cpp
+++ ProjectModernpp/GameMap.cpp
@@ -1,1 +1,43 @@
 #include "GameMap.h"
+
+GameMap::GameMap(uint32_t width, uint32_t height)
+    : m_width(width),
+    m_height(height)
+{
+    m_grid.resize(height, std::vector<CellType>(width, CellType::EMPTY));
+}
+
+void GameMap::initialize(uint8_t level) {
+
+    srand(static_cast<unsigned>(time(0)));
+
+    int breakableWallChance;
+    int unbreakableWallChance;
+    // Set wall chances based on difficulty level
+    switch (level) {
+    case 1: // easy 
+        breakableWallChance = 10;
+        unbreakableWallChance = 5;
+    case 2: // medium
+        breakableWallChance = 20;
+        unbreakableWallChance = 10;
+    case 3: // hard
+        breakableWallChance = 30;
+        unbreakableWallChance = 15;
+    default:
+        break;
+    }
+
+    for (int i = 0; i < m_height; ++i) {
+        for (int j = 0; j < m_width; ++j) {
+            int randVal = rand() % 100;
+            if (randVal < unbreakableWallChance)
+                // Set cell as BREAKABLE_WALL 
+                m_grid[i][j] = CellType::BREAKABLE_WALL;
+            else if (randVal < breakableWallChance + unbreakableWallChance)
+                // Set cell as UNBREAKABLE_WALL 
+                m_grid[i][j] = CellType::UNBREAKABLE_WALL;
+            //Defaut - EMPTY 
+        }
+    }
+}
--- ProjectModernpp/GameMap.h
+++ ProjectModernpp/GameMap.h
@@ -7,32 +7,20 @@
 class GameMap
 {
 private:
-	uint32_t m_width, m_height;
+    uint32_t m_width;
+    uint32_t m_height;
+    uint8_t m_level; // easy = 1, medium = 2, hard = 3
+
     // 2D grid representing the map layout
-	std::vector<std::vector<CellType>> m_grid;
+    std::vector<std::vector<CellType>> m_grid;
 
 public:
     // Constructor
-	GameMap(int width, int height) : m_width(width), m_height(height) {
-		m_grid.resize(height, std::vector<CellType>(width, CellType::EMPTY));
-	}
+    GameMap(uint32_t width, uint32_t height);
+
 
     // Initializes the grid with random cell types; default - Empty
-    void initialize() {
-        srand(static_cast<unsigned>(time(0)));
-        for (int i = 0; i < m_height; ++i) {
-            for (int j = 0; j < m_width; ++j) {
-                int randVal = rand() % 10;
-                if (randVal < 3)
-                    // Set cell as BREAKABLE_WALL with 30% probability
-                    m_grid[i][j] = CellType::BREAKABLE_WALL;
-                else if (randVal < 5) 
-                    // Set cell as UNBREAKABLE_WALL with 20% probability
-                    m_grid[i][j] = CellType::UNBREAKABLE_WALL;
-                //Defaut - EMPTY - 50% probability
-            }
-        }
-    }
+    void initialize(uint8_t level);
 
 
 };
