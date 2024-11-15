d66a3bae75e7ec3d55a84bd785bfbb10c1e6a73e
iuliap710 <111219535+iuliap710@users.noreply.github.com>
2024-11-15 19:11:03 +0200

    I wrote in Game.h.

--- ProjectModernpp/Game.h
+++ ProjectModernpp/Game.h
@@ -1,5 +1,11 @@
 #pragma once
 
 class Game {
+public:
+    GameMap map;
+    Player player;
 
+    Game(uint32_t width, uint32_t height, uint16_t level, const std::string& name)
+        : map(width, height, level), player(name) {
+    }
 };
