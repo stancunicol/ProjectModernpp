243200f60ba48dc0c491168095505b24806edf87
Ingrid2911 <ingrid.mihaita@gmail.com>
2024-11-20 14:51:24 +0200

    I recreated PlayWindow class.

---
+++ ProjectModernpp/Interface/PLayWindow.cpp
@@ -0,0 +1,84 @@
+﻿#include "PLayWindow.h"
+#include <QVBoxLayout>
+#include <QPushButton>
+#include<qpalette>
+PlayWindow::PlayWindow(QWidget* parent)
+    : QWidget(parent) {
+    // Set title and size for window
+    setWindowTitle("Generate Code");
+    setFixedSize(300, 200);
+
+
+    QPalette palette;
+    palette.setColor(QPalette::Window, Qt::black); // Background
+    palette.setColor(QPalette::WindowText, Qt::white); // Text 
+    setPalette(palette);
+    setAutoFillBackground(true); 
+
+    // Creating  layout
+    QVBoxLayout* layout = new QVBoxLayout(this); 
+    GenerateCode = new QLineEdit(this);
+    GenerateCode->setPlaceholderText("CODE");
+    GenerateCode->setAlignment(Qt::AlignCenter);
+    GenerateCode->setEnabled(false);
+    GenerateCode->setStyleSheet("color: lightBlue;background-color: darkBlue;font-size: 20px;border: 2px solid lightBlue");
+
+    GenerateCode->setFixedSize(240, 50);
+
+    layout->addWidget(GenerateCode);
+    layout->setAlignment(Qt::AlignCenter); 
+
+
+    // Create generate code button
+    GenerateButton = new QPushButton("Generate code", this);
+    GenerateButton->setStyleSheet("background-color: blue;"
+        "color: lightBlue;"
+        "font-weight: bold;"
+        "border-radius: 5px;"
+        "font-size: 15px;"
+        "padding: 10px; "
+        "border: 2px solid drakBlue; ");
+    layout->addWidget(GenerateButton);
+
+
+
+    FowardButton = new QPushButton("Start game", this);
+    FowardButton->setStyleSheet("background-color:green;"
+        "color:lightGreen;"
+        "font-weight: bold;"
+        "border-radius: 5px;"
+        "font-size: 15px;"
+        "padding: 10px;"
+        "border: 2px solid darkGreen; ");
+    layout->addWidget(FowardButton);
+    connect(GenerateButton, &QPushButton::clicked, this, &PlayWindow::onGenerateClicked);
+    connect(FowardButton, &QPushButton::clicked, this, &PlayWindow::onGenerateClicked);
+   
+
+
+
+}
+
+PlayWindow::~PlayWindow() {
+    // Destructor (nimic special de curățat aici)
+}
+QString PlayWindow::GenerateRandomNumber()
+{
+    QString number;
+
+    // Generarea a 5 cifre aleatoare
+    srand(time(NULL));
+    for (int i = 0; i < 5; ++i) {
+
+        int digit = rand() % 10; // Generare cifră între 0 și 9
+        number.append(QString::number(digit)); // Adăugăm cifra la string
+    }
+    return number;
+
+}
+
+void PlayWindow::onGenerateClicked() {
+    QString randomNumber = GenerateRandomNumber();
+    GenerateCode->setText(randomNumber);
+}
+
---
+++ ProjectModernpp/Interface/PLayWindow.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QMessageBox> // Pentru afișarea mesajelor de eroare
+
+class PlayWindow : public QWidget {
+    Q_OBJECT
+
+public:
+    PlayWindow(QWidget* parent = nullptr); // Constructor
+    ~PlayWindow(); // Destructor
+
+protected:
+    void onGenerateClicked(); // Slot pentru gestionarea click-ului pe butonul de generare cod
+    QString GenerateRandomNumber();// functie pentru generare random de cod
+
+private:
+    QLineEdit* GenerateCode; // Câmp pentru numele de utilizator
+    QPushButton* GenerateButton; // Buton pentru logare
+    QPushButton* FowardButton; //Buton pentru mers mai departe
+    QLabel* messageLabel; // Etichetă pentru mesajele de feedback
+};
+
