e4c8ef1dcaa7ed43bc081a62bfe33f65e82f6bcb
Ingrid2911 <ingrid.mihaita@gmail.com>
2024-11-27 16:09:56 +0200

    I created ConnectWindow class

---
+++ ProjectModernpp/Interface_client/ConnectWindow.h
@@ -0,0 +1,24 @@
+﻿#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QMessageBox> // Pentru afișarea mesajelor de eroare
+
+class ConnectWindow : public QWidget {
+    Q_OBJECT
+
+public:
+    ConnectWindow(QWidget* parent = nullptr); // Constructor
+    ~ConnectWindow(); // Destructor
+
+protected:
+    void OnGenerateClicked(); // Slot pentru gestionarea click-ului pe butonul de generare cod
+
+private:
+    QLineEdit* insertCode; // Câmp pentru numele de utilizator
+    QPushButton* startButton; //Buton pentru mers mai departe
+    QLabel* messageLabel; // Etichetă pentru mesajele de feedback
+
+};
+
