25caf6c0d1b415365d726f0ffaf54c855f04a0b1
Ingrid2911 <ingrid.mihaita@gmail.com>
2024-11-27 16:12:13 +0200

    I wrote in ConnectWindow.cpp

---
+++ ProjectModernpp/Interface_client/ConnectWindow.cpp
@@ -0,0 +1,28 @@
+﻿#include "ConnectWindow.h"
+#include <QVBoxLayout>
+#include <QPushButton>
+#include<qpalette>
+ConnectWindow::ConnectWindow(QWidget* parent)
+    : QWidget(parent) {
+
+    // Setare titlu și dimensiune fereastră
+    setWindowTitle("Join Room");
+    setFixedSize(300, 200);
+
+
+    QPalette palette;
+    palette.setColor(QPalette::Window, Qt::black); // Fundal negru
+    palette.setColor(QPalette::WindowText, Qt::white); // Text alb
+    setPalette(palette);
+    setAutoFillBackground(true); // Asigură-te că fundalul se umple cu culoarea
+
+
+    QVBoxLayout* layout = new QVBoxLayout(this);
+    QLabel* label = new QLabel("Insert code:", this);
+    QFont font;
+    font.setPointSize(14);  // Dimensiunea fontului
+    font.setFamily("Segoe Script");  // Familia fontului
+    font.setBold(true);  // Font îngroșat (opțional)
+
+    label->setFont(font);
+    layout->addWidget(label);
