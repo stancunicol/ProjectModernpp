be487055125e44f4b63226a3f384474e44ee1b6c
Alessandra <vilcualessandra@gmail.com>
2024-12-10 19:47:37 +0200

    I wrote in ServerUtils.cpp

--- ProjectModernpp/Interface_client/ServerUtils.cpp
+++ ProjectModernpp/Interface_client/ServerUtils.cpp
@@ -5,5 +5,35 @@
 #include <string>
 
 void SendLevelToServer(int level) {
-    
+    CURL* curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl) {
+        std::string url = "http://localhost:8080/submitLevel";
+        std::string jsonPayload = "{\"level\": " + std::to_string(level) + "}";
+
+        struct curl_slist* headers = nullptr;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonPayload.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            qDebug() << "Failed to send level: " << curl_easy_strerror(res);
+        }
+        else {
+            qDebug() << "Level sent successfully: " << level;
+        }
+
+        curl_slist_free_all(headers);
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
 }
