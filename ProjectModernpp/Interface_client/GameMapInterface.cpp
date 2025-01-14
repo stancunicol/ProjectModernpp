9480b851c557970cf1a398fd22f433d9aeeb4b47
Ingrid2911 <ingrid.mihaita@gmail.com>
2025-01-14 16:05:56 +0200

    I delete the modifications.

--- ProjectModernpp/Interface_client/GameMapInterface.cpp
+++ ProjectModernpp/Interface_client/GameMapInterface.cpp
@@ -10,38 +10,10 @@ GameMapInterface::GameMapInterface(QWidget* parent)
 {
     setWindowTitle("Game Map");
     setFixedSize(600, 600);
-    ServerUtils server;
-    scoreUpdateTimer = new QTimer(this);
-
     
-    for (int i = 1; i <= 4; i++)
-    {
-        std::string response = m_serverObject.GetPlayerDataByIdFromServer();
-
-        if (!response.empty()) {
-            try {
-                auto jsonResponse = nlohmann::json::parse(response);
-                if (jsonResponse.contains("name") && jsonResponse.contains("score")) {
-                    std::string playerName = jsonResponse["name"];
-                    int score = jsonResponse["score"];
-                    playerNames.push_back(playerName);
-                    playerScores.push_back(score);
-
-                }
-            }
-            catch (const std::exception& e) {
-                qDebug() << "Error parsing server response: " << e.what();
-            }
-        }
-    }
-    if (scoreUpdateTimer != nullptr) {
-        connect(scoreUpdateTimer, &QTimer::timeout, this, &GameMapInterface::updateScores);
-        scoreUpdateTimer->start(10000); 
-    }
-    else {
-        qDebug() << "Error: scoreUpdateTimer is nullptr.";
-    }
-    /*std::pair<int, int> basePosition = m_serverObject.GetBaseFromServer();
+    
+    
+    std::pair<int, int> basePosition = m_serverObject.GetBaseFromServer();
     qDebug() << "Base Position - X:" << basePosition.first << " Y:" << basePosition.second;
 
     std::vector<Bomb> bombs = m_serverObject.GetBombsFromServer();
@@ -58,7 +30,7 @@ GameMapInterface::GameMapInterface(QWidget* parent)
     qDebug() << "Enemy Positions:";
     for (const auto& enemy : enemies) {
         qDebug() << "Enemy ID:" << enemy.id << "Position: (" << enemy.x << "," << enemy.y << ")";
-    }*/
+    }
 }
 
 GameMapInterface::~GameMapInterface() {
@@ -105,52 +77,7 @@ void GameMapInterface::keyPressEvent(QKeyEvent* event)
         QMainWindow::keyPressEvent(event);
     }
 }
-void GameMapInterface::paintEvent(QPaintEvent* event)
-{
 
-    QPainter painter(this);
-    painter.setFont(QFont("Segoe Script", 8));
-
-    int startX = 15; //initial position on the x axis
-    int startY = 15; // initial position on the y axis
-    int spacing = 90; // the distance between players
-
-    
-    for (int i = 0; i < 4; i++) {
-        // draw the name
-        painter.drawText(startX + i * spacing, startY, QString::fromStdString(playerNames[i]));
-
-        // draw the score
-        painter.drawText(startX + i * spacing, startY + 20, "Score: " + QString::number(playerScores[i]));
-    }
-}
-void GameMapInterface::updateScores()
-{
-    ServerUtils server;
-    qDebug() << "Updating scores...";
-
-    for (int i = 0; i < static_cast<int>(playerScores.size()); ++i) {
         
-        std::string response = server.GetPlayerDataByIdFromServer(); 
+    
 
-        if (!response.empty()) {
-            try {
-                auto jsonResponse = nlohmann::json::parse(response);
-
-                if (jsonResponse.contains("score")) {
-                    int newScore = jsonResponse["score"];
-
-                    // update the score only if it has changed
-                    if (playerScores[i] != newScore) {
-                        playerScores[i] = newScore;
-                        qDebug() << "Player" << i + 1 << "score updated to:" << newScore;
-                        update(); 
-                    }
-                }
-            }
-            catch (const std::exception& e) {
-                qDebug() << "Error parsing server response: " << e.what();
-            }
-        }
-    }
-}
--- ProjectModernpp/Interface_client/GameMapInterface.h
+++ ProjectModernpp/Interface_client/GameMapInterface.h
@@ -12,14 +12,6 @@ public:
     GameMapInterface(QWidget* parent = nullptr);
     ~GameMapInterface();
     void keyPressEvent(QKeyEvent* event);
-private slots:
-    void updateScores(); // Slot for update the scores
 private:
     ServerUtils m_serverObject;
-    std::vector<std::string> playerNames;
-    std::vector<int> playerScores;
-    QTimer* scoreUpdateTimer; // Timer
-protected:
-    void paintEvent(QPaintEvent* event) override;
-
 };
